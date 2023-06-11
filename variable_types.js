1dccc32b715e42cf5dd4a4830a14df2dfc5f7e50
Kaushik <kaushik2mail@gmail.com>
2023-06-11 12:40:21 +0530

    Variables Types

---
+++ variable_types.js
@@ -0,0 +1,8 @@
+let myAge = 17;
+let Name = "Kaushik";
+let condition = 6 < 3;
+let NumArray = [10, 15, 21];
+console.log(myAge);
+console.log(Name);
+console.log(condition);
+console.log(NumArray);
